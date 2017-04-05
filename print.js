6952634e1963ce9db3de8528435f90720520f7cb
xiaomingTang <1038761793@qq.com>
2017-04-05 18:16:38 +0800

    功能改进

--- print.js
+++ print.js
@@ -8,11 +8,19 @@ function strToArr(str){
   for(var i=0;i<len;i++){
     arr.push(str.charAt(i));
   }
+  arr.push("&nbsp;");
   return arr;
 }
 
-function displayIn(str,elem){
-  var tempArr=strToArr(str),
+//顺序输出arr字符串到elem元素
+//应用时无须给出num参数
+//如果给出num参数,则输出从arr[num]之后的元素
+//为了输出美观,每个元素输出时,结尾均多了一个空格
+function displayIn(arr,elem,num){
+  if(!num){
+	  num=0;
+  }
+  var tempArr=strToArr(arr[num]),
       i=0,
       len=tempArr.length,
   loop=setInterval(function(){
@@ -20,10 +28,13 @@ function displayIn(str,elem){
     if(i>=len){
       clearInterval(loop);
       elem.innerHTML+="\<br \/\>";
+	  if(num++<arr.length){
+		  displayIn(arr,elem,num);
+	  }
     }
-  },500);
+  },300);
 }
 
+var arr=["hello world!","thanks for your review","要输出的字符以数组形式存储","一个元素占据一行依次输出"];
 
-displayIn("hello world!",$("test"));
-
+displayIn(arr,$("print"));
